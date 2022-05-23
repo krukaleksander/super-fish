2c9d68cd14c10470ddaf2a2de83248eb1d834e31
aleksander <kruk.aleksander@hotmail.com>
2022-05-23 15:21:16 +0200

    feat: add jest e2e config file

---
+++ packages/backend/jest-e2e.config.ts
@@ -0,0 +1,16 @@
+module.exports = {
+  displayName: 'backend',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/server',
+  testMatch: ['./**/*.e2e-spec.ts'],
+};
