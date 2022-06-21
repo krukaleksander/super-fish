6505cc91ad399c21b0038d60e86d97a4c2703ac4
aleksander <kruk.aleksander@hotmail.com>
2022-06-21 08:22:10 +0200

    chore: create database provider

---
+++ packages/backend/src/database/database.providers.ts
@@ -0,0 +1,20 @@
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'postgres',
+        database: 'postgres',
+        entities: [__dirname + '/entities/*.entity{.ts,.js}']
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
