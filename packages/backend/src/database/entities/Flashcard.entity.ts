1e270e38e593ff284beae44566102014943430d4
aleksander <kruk.aleksander@hotmail.com>
2022-06-21 08:13:33 +0200

    chore: create flash card entity

---
+++ packages/backend/src/database/entities/Flashcard.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, ManyToOne } from "typeorm";
+import { IFish } from "../../interfaces";
+import { JoinColumn } from "typeorm/browser";
+import { FishPackage } from "./FishPackage.entity";
+
+@Entity({name: 'flashcard'})
+export class FlashCard implements IFish{
+  @Column({unique: true})
+  id: string;
+  @Column()
+  backSide: string;
+  @Column()
+  createdAt: string;
+  @Column()
+  frontSide: string;
+  @Column()
+  packageID: string;
+  @Column()
+  refreshedAt: string;
+  @Column()
+  remember: boolean;
+  @ManyToOne(() => FishPackage, (fishpackage) => fishpackage.cards)
+  @JoinColumn({referencedColumnName: 'id', name: 'packageId'})
+  package: FishPackage
+}
