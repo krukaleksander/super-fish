246075a069233f8fbd6a759cf0e177b87da8630a
aleksander <kruk.aleksander@hotmail.com>
2022-06-21 08:25:50 +0200

    chore: import database module to app module

--- packages/backend/src/app/app.module.ts
+++ packages/backend/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {FishModule} from "../fish/fish.module";
+import { DatabaseModule } from "../database/database.module";
 
 @Module({
-  imports: [FishModule],
+  imports: [FishModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
